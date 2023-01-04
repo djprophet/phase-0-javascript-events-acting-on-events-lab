bb58836e55e82e5a1a4160aa0999cb01c8de8767
Demarcus <demarcusprophet@gmail.com>
2023-01-04 02:22:45 -0600

    commit tree 4f28bf7b76a86fa5d614151b3eb45a63a49c3914

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+function moveDodgerLeft() {
+    var dodger = document.getElementById('dodger');
+    var left = parseInt(dodger.style.left);
+    dodger.style.left = (left - 10) + 'px';
+  }
+  
+  function moveDodgerRight() {
+    var dodger = document.getElementById('dodger');
+    var left = parseInt(dodger.style.left);
+    dodger.style.left = (left + 10) + 'px';
+  }
